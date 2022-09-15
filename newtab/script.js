62ad1d29391bb7f7fe4a029ee3efddb9753047fb
kouseralamin <kouseralamin@gmail.com>
2022-09-15 09:39:19 +0600

    removed: message

--- newtab/script.js
+++ newtab/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function main() {
+function updateData() {
     window.document.getElementsByClassName("HEADER")[0].getElementsByClassName("SPOTLIGHT")[0].getElementsByTagName("ul")[0].innerHTML = "";
     const spotlight = JSON.parse(window.localStorage.getItem("bsmrmuNewTabData")).spotlight;
     for (let i = 0; i < spotlight.length; i++) {
@@ -15,36 +15,34 @@ function main() {
     }
 }
 if (window.localStorage.getItem("bsmrmuNewTabData") !== null) {
-    main();
+    updateData();
 }
+bsmrmuSetNewTabData();
 
-chrome.runtime.sendMessage(null, function (_) {});
-
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (sender.tab === undefined && request.length === 3) {
-            sendResponse(true);
-            if (request !== false) {
-                bsmrmuParseAndSetNewTabData(request);
-            }
-        };
-    }
-);
-
-function bsmrmuParseAndSetNewTabData(data) {
+function bsmrmuSetNewTabData() {
     const bsmrmuNewTabData = {
         "spotlight": [],
         "general_notice": [],
         "academic_notice": []
     }
-    const parser = new DOMParser();
-    const doc0 = parser.parseFromString(data[0], "text/html").getElementById("carouselExampleIndicators").getElementsByClassName("carousel-inner")[0].getElementsByTagName("a");
-    for (let i = 0; i < doc0.length; i++) {
-        bsmrmuNewTabData.spotlight.push([
-            doc0[i].innerText.trim(),
-            doc0[i].href
-        ])
-    }
-    window.localStorage.setItem("bsmrmuNewTabData", JSON.stringify(bsmrmuNewTabData));
-    main();
+    Promise.all([
+        fetch("https://bsmrmu.edu.bd/").then(function (value) { return value.text() }).catch(function (_) { return false }),
+        fetch("https://bsmrmu.edu.bd/notices/General%20Notice").then(function (value) { return value.text() }).catch(function (_) { return false }),
+        fetch("https://bsmrmu.edu.bd/notices/Academic%20Notice").then(function (value) { return value.text() }).catch(function (_) { return false })
+    ]).then(function (value) {
+        if (!value.includes(false)) {
+            const parser = new DOMParser();
+            const doc0 = parser.parseFromString(value[0], "text/html").getElementById("carouselExampleIndicators").getElementsByClassName("carousel-inner")[0].getElementsByTagName("a");
+            for (let i = 0; i < doc0.length; i++) {
+                bsmrmuNewTabData.spotlight.push([
+                    doc0[i].innerText.trim(),
+                    doc0[i].href
+                ])
+            }
+            window.localStorage.setItem("bsmrmuNewTabData", JSON.stringify(bsmrmuNewTabData));
+            updateData();
+        }
+    }).catch(function () {
+        console.error("function: bsmrmuSetNewTabData()");
+    })
 }
